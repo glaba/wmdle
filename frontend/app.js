9fe3e8ae78ccdad1090c432370cd20a942bba6db
Rahulraj-Rajkumar <rahulraj.rajkumar@gmail.com>
2024-05-07 22:10:09 -0700

    Disable submit button on guess + error handling

--- frontend/app.js
+++ frontend/app.js
@@ -1,23 +1,35 @@
 const guessForm = document.getElementById('guess-form');
 const adminForm = document.getElementById('admin-form');
+const guessSubmitButton = document.getElementById('guessSubmitButton');
 
 guessForm.addEventListener('submit', async (event) => {
   event.preventDefault();
+  guessSubmitButton.disabled = true; 
+  guessSubmitButton.style.backgroundColor = '#ccc';
   const formData = new FormData(guessForm);
   const data = {
     name : formData.get('name'),
     email : formData.get('email'),
     guess : formData.get('guess')
   };
-  await fetch('/api/guess', {
-    method : 'POST',
-    headers : {'Content-Type' : 'application/json'},
-    body : JSON.stringify(data)
-  });
-  document.getElementById('name').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('guess').value = '';
-  alert('Your guess has been submitted!');
+
+  try {
+    const response = await fetch('/api/guess', {
+      method : 'POST',
+      headers : {'Content-Type' : 'application/json'},
+      body : JSON.stringify(data)
+    });
+    if (response.ok != true) {
+      throw new Error(`Something wrong, status: ${response.status}`);
+    }
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('guess').value = '';
+    alert('Your guess has been submitted!');
+  } catch (error) {
+    console.error('Error:', error);
+    alert('BBL Drizzy, BBL Drizzzzyyy');
+  }
 });
 
 adminForm.addEventListener('submit', async (event) => {
